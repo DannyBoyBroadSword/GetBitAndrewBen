bdc0968e3ac152b54e0d0ca5454ff09b5e00bc88
Andrew Hennessy <andrew@andrewhennessy.net>
2015-04-13 17:44:59 -0400

    made predictive engine and previous card handler.

--- GetBitBenAndrew/src/Rich_Homie_Quan.py
+++ GetBitBenAndrew/src/Rich_Homie_Quan.py
@@ -7,6 +7,10 @@ NEED TO FIND MATH TO FIND OUR STRATEGY
 '''
 from robot import *;
 from findertools import move
+from get_bit import*;
+
+#Confirm that we can reference robot names with self definition. 
+# robot self.get_limbs(self)
 
 
 
@@ -14,47 +18,84 @@ class Rich_Homie_Quan(Robot):
     '''
     classdocs
     '''
+    def __init__(self, score,hand,previousCard,predictedScore,*robots):
+        for robot in self.robots:
+            self.score = self.scoreStart()
+            self.hand = self.get_hands()
+            self.previousCard = None;
+            self.predictedScore = None;
    
      
     def get_move(self):
         #Essentially made everything self to enable acces of all functions for each player. 
         if self.Robot.start() == True:
             self.startGame()
+        for robot in self.robots:
+            self.engine(robot)
         #MainCallEngine.
     
+    def previousCard(self):
+        for robot in self.robots:
+            self.attachHand(robot)
     
     def game_start(self):
         #please iterate through all players. 
-        Rich_Homie_Quan.score = Rich_Homie_Quan.getScore(self)
+        self.cards_played = []
+        for robot in self.robots:
+            self.score = self.startScore()
+            
+    def engine(self,robot):
+        return self.getScore()
         
-    def card_refresh(self,move): #call this on refresh after death or stuff. 
-        # this will have to be managed for each player. 
-        self.move = move
-        self.CardsPlayed.append(move)
+            
+        
+
+        
         
     def handScore(self):
-        self.get_hands()
+        self.cardMargin = self.previousCard[:0]-self.previousCard[:-1]
+        return self.cardMargin*20
         
-    def previousCard(self):
-        #get players previous card
-        pass
+    def attachHand(self,robot):
+        self.robot.get_hands()
+        
+    def get_previousCard(self):
+        for card in range(len(5)):
+            if card not in self.hand:
+                self.previousCard.append(card)  
+      
     
     def limbs(self):
-        return Robot.get_limbs(self)
+        return self.get_limbs
     
     def position(self):
         return Robot.get_order(self).index(self)
     
     def getScore(self):
-        return self.score
+        return self.score()
     
+    def predictiveScore(self):
+        self.thoseScores = []
+        self.average = None
+        for cards in self.hand:
+            self.thoseScores.append(self.predictionEngine(cards))
+        self.highest = max(self.thoseScores)
+        if len(self.thoseScores)>0:
+            for scores in range(len(self.thoseScores)):
+                self.average += scores[self.thoseScores]
+            self.average = self.average/len(self.thoseScores)
+            if self.average<self.highest:
+                return (self.average+self.highest)/(self.thoseScores+1)
+        else:
+            return self.highest
+                                  
+    def predictingHandScore(self,card):
+        self.cardMargin = self.previousCard[:0]-self.previousCard[:-1]
+        return self.cardMargin*20
         
-
-
-class StrategyEvaluate(Rich_Homie_Quan(Robot)):
-    
-    #def __init__(self,end=False,start=True,mid=False):
-        
+    def predictionEngine(self,card):
+        ((self.PreviousScores[:-1])+(100/(self.limbs()) + 100/(self.position()+1) + self.predictingHandScore(card)))
+            
         
     #TODO evaluate against out stategy takes in inputs from stat handler and advance to change and autotune strategy
     
@@ -65,9 +106,11 @@ class StrategyEvaluate(Rich_Homie_Quan(Robot)):
     def scoreStart(self):
         self.previousPositions.append(self.position())
         return 100/(self.limbs() + 100/self.position+1) + 100
-        
+    
     def score(self):
         self.PreviousScores.append(100/(self.limbs()) + 100/(self.position()+1) + (self.handScore()))    
-        self.score = 100/(self.limbs()) + 100/(self.position()+1) + 100/(self.handScore())
-        
+        self.score = 100/(self.limbs()) + 100/(self.position()+1) + self.handScore()
+        return self.score
+   
+   
    
