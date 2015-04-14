87a9dfe170364730f7c8e954bcda557ae3204e9f
Andrew Hennessy <andrew@andrewhennessy.net>
2015-04-13 20:50:57 -0400

    Blocked out different Classes
    wondering if that messes up with the self and arbitrary methods in order to calculate otherpeoples suttf.

--- GetBitBenAndrew/src/Rich_Homie_Quan.py
+++ GetBitBenAndrew/src/Rich_Homie_Quan.py
@@ -18,13 +18,32 @@ class Rich_Homie_Quan(Robot):
     '''
     classdocs
     '''
+    
+class initialization(Rich_Homie_Quan(Robot)):
+    
     def __init__(self, score,hand,previousCard,predictedScore,*robots):
         for robot in self.robots:
             self.score = self.scoreStart()
             self.hand = self.get_hands()
             self.previousCard = None;
             self.predictedScore = None;
+            
+    def startGame(self, start):
+        pass
+        
+    def scoreStart(self):
+        self.previousPositions.append(self.position())
+        return 100/(self.limbs() + 100/self.position+1) + 100
+
+class moveManager(Rich_Homie_Quan(Robot)):
    
+    def on_move(self):
+        self.hand = self.get_hands()
+        pass
+
+    def execute_move(self):
+        pass
+    
      
     def get_move(self):
         #Essentially made everything self to enable acces of all functions for each player. 
@@ -33,37 +52,41 @@ class Rich_Homie_Quan(Robot):
         for robot in self.robots:
             self.engine(robot)
         #MainCallEngine.
-    
-    def previousCard(self):
-        for robot in self.robots:
-            self.attachHand(robot)
-    
+        
     def game_start(self):
         #please iterate through all players. 
         self.cards_played = []
         for robot in self.robots:
             self.score = self.startScore()
-            
-    def engine(self,robot):
-        return self.getScore()
-        
-            
-        
+    
 
+class MainExecutionThread(Rich_Homie_Quan(Robot)):
+    pass
         
+class cardManager(Rich_Homie_Quan(Robot)):
         
-    def handScore(self):
-        self.cardMargin = self.previousCard[:0]-self.previousCard[:-1]
-        return self.cardMargin*20
-        
+   
+    def previousCard(self):
+        for robot in self.robots:
+            self.attachHand(robot)
+
+
     def attachHand(self,robot):
         self.robot.get_hands()
         
     def get_previousCard(self):
-        for card in range(len(5)):
-            if card not in self.hand:
+        for card in range(len(Robot.get_order(self))):
+            if self.hand.count(card) == 0  and self.hand.count(self.previousCard):
                 self.previousCard.append(card)  
       
+      
+class scoring(Rich_Homie_Quan(Robot)):
+    
+    
+    def handScore(self):
+        self.cardMargin = self.previousCard[-1]-self.previousCard[-2]
+        return self.cardMargin*(100/len(Robot.get_order(self)))
+        
     
     def limbs(self):
         return self.get_limbs
@@ -73,6 +96,13 @@ class Rich_Homie_Quan(Robot):
     
     def getScore(self):
         return self.score()
+    
+    def score(self):
+        self.PreviousScores.append(100/(self.limbs()) + 100/(self.position()+1) + (self.handScore()))    
+        self.score = 100/(self.limbs()) + 100/(self.position()+1) + self.handScore()
+        return self.score
+    
+class predictionEngine(Rich_Homie_Quan(Robot)):
     
     def predictiveScore(self):
         self.thoseScores = []
@@ -85,32 +115,21 @@ class Rich_Homie_Quan(Robot):
                 self.average += scores[self.thoseScores]
             self.average = self.average/len(self.thoseScores)
             if self.average<self.highest:
-                return (self.average+self.highest)/(self.thoseScores+1)
+                return (self.average+self.highest)/(len(self.thoseScores)+1)
         else:
             return self.highest
                                   
     def predictingHandScore(self,card):
-        self.cardMargin = self.previousCard[:0]-self.previousCard[:-1]
-        return self.cardMargin*20
+        self.cardMargin = self.previousCard[-1]-self.previousCard[-2]
+        return self.cardMargin*(100/len(Robot.get_order(self)))
         
     def predictionEngine(self,card):
-        ((self.PreviousScores[:-1])+(100/(self.limbs()) + 100/(self.position()+1) + self.predictingHandScore(card)))
+        return ((100/(self.limbs()) + 100/(self.position()+1) + self.predictingHandScore(card)))
             
-        
-    #TODO evaluate against out stategy takes in inputs from stat handler and advance to change and autotune strategy
     
-    def startGame(self, start):
-        #ikr
-        pass
-        
-    def scoreStart(self):
-        self.previousPositions.append(self.position())
-        return 100/(self.limbs() + 100/self.position+1) + 100
+  
     
-    def score(self):
-        self.PreviousScores.append(100/(self.limbs()) + 100/(self.position()+1) + (self.handScore()))    
-        self.score = 100/(self.limbs()) + 100/(self.position()+1) + self.handScore()
-        return self.score
-   
-   
-   
+class cleanup(Rich_Homie_Quan(Robot)):
+    
+    def end_game(self):
+        pass   
